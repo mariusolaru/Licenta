64435a572e47ab57cef53fef51e6379d22af6906
mariusolaru <mariusolaru1997@yahoo.com>
2018-03-17 21:05:35 +0200

    Created header

--- Alumni/src/app/app.module.ts
+++ Alumni/src/app/app.module.ts
@@ -7,11 +7,13 @@ import { AppComponent } from './app.component';
 
 import { CollapseModule, BsDropdownModule } from 'ngx-bootstrap';
 import { ApploginNavbarComponent } from './applogin-navbar/applogin-navbar.component';
+import { HeaderComponent } from './header/header.component';
+import { SidebarComponent } from './sidebar/sidebar.component';
 
 @NgModule({
   imports:      [ BrowserModule, FormsModule, CollapseModule.forRoot(), BsDropdownModule.forRoot(),
     NgbModule.forRoot() ],
-  declarations: [ AppComponent, ApploginNavbarComponent ],
+  declarations: [ AppComponent, ApploginNavbarComponent, HeaderComponent, SidebarComponent ],
   bootstrap:    [ AppComponent ]
 })
 export class AppModule { }
---
+++ Alumni/src/app/header/header.component.ts
@@ -0,0 +1,35 @@
+import { Component, OnInit } from '@angular/core';
+// import { Router, NavigationEnd } from '@angular/router';
+//import { TranslateService } from '@ngx-translate/core';
+
+@Component({
+    selector: 'app-header',
+    templateUrl: './header.component.html',
+    styleUrls: ['./header.component.scss']
+})
+export class HeaderComponent implements OnInit {
+    pushRightClass: string = 'push-right';
+
+    constructor() { }
+
+    ngOnInit() {}
+
+    isToggled(): boolean {
+        const dom: Element = document.querySelector('body');
+        return dom.classList.contains(this.pushRightClass);
+    }
+
+    toggleSidebar() {
+        const dom: any = document.querySelector('body');
+        dom.classList.toggle(this.pushRightClass);
+    }
+
+    rltAndLtr() {
+        const dom: any = document.querySelector('body');
+        dom.classList.toggle('rtl');
+    }
+
+    onLoggedout() {
+        localStorage.removeItem('isLoggedin');
+    }
+}
